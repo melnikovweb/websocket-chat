8f293bfeaaa8b60605c045340c1881604e1f20ca
Andrew Melnikov <melnikovweb@gmail.com>
2022-11-28 01:44:47 +0200

    logs_doesnt_work

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
-
-import { WebSocketServer } from "ws";
+import { WebSocketServer } from 'ws'
 import { v4 as uuid } from 'uuid'
+import { writeFile, readFileSync, existsSync } from 'fs'
 
 const clients = {}
-
-const wss = new WebSocketServer({port: 9000});
+const log = existsSync('log') && readFileSync('log')
+// const messages = JSON.parse(log) || []
+const messages = []
+const wss = new WebSocketServer({ port: 9000 })
 wss.on('connection', ws => {
   const id = uuid()
   clients[id] = ws
   console.log(`New client ${id}`)
-  ws.on('message', rawMessage => {})
+  ws.send(JSON.stringify(messages))
+  ws.on('message', rawMessage => {
+    const { name, message } = JSON.parse(rawMessage)
+    messages.push({ name, message })
+    for (const id in clients) {
+      clients[id].send(JSON.stringify([{ name, message }]))
+    }
+  })
   ws.on('close', () => {
     delete clients[id]
     console.log(`client ${id} is closed`)
   })
 })
 
+process.on('SIGINT', () => {
+  wss.close()
+  console.log(messages)
+  writeFile('log', JSON.stringify(messages), err => {
+    if (err) {
+      console.log(err)
+    }
+    process.exit()
+  })
+})
